ccf3cbdb7c48ea626b69cca4d4ab79cf830c4b8f
vladyslav <vladyslav.tsyhanenco@gmail.com>
2022-12-27 23:07:13 +0200

    added carousel

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,22 @@ const hello = () => {
   
   hello()
     .then(value => console.log(value));
+
+$(document).ready(function(){
+  $('.carousel__slider').slick({
+    speed: 700,
+    adaptiveHeight: false,
+    prevArrow: '<button type="button" class="slick-prev"><img src="src/img/carousel/chevron-left.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="src/img/carousel/chevron-right.png"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
+  });
+});
+            
